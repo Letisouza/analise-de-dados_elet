43b51b787ca8b0145b0ce9b8257575183dc73638
Leticia Souza <leticia.suely@ufpe.br>
2021-04-12 21:26:14 -0300

    Aula 3 e Atividade

--- aula 3.R
+++ aula 3.R
@@ -15,3 +15,14 @@ instituicao <- c("UFPE", "Católica", "Estácio", "UFPE")
 perfil_alunos <- data.frame(nome, idade, cidade, curso, instituicao)
 str(perfil_alunos)
 head(perfil_alunos)
+
+# Criando variáveis
+
+dnormal <- rnorm(150)
+dnormal
+
+dbinomial <- rbinom(150, 1, 0.3)
+dbinomial
+
+index <- seq(1, length(dbinomial))
+index
