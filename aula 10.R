b8b111197096b973efde859d702c5c716f28180a
Leticia Souza <leticia.suely@ufpe.br>
2021-05-31 17:22:32 -0300

    Aula 10 e Atividade

--- aula 10.R
+++ aula 10.R
@@ -12,3 +12,27 @@ raca <- factor(raca, levels = recode, labels = names(recode))
 
 ######################################
 
+library(ade4)
+library(arules)
+library(poliscidata)
+
+
+str(world)
+
+# Seleção de colunas
+worldselect <- world[ ,1:30]
+# Dessa seleção, separar colunas que são fatores para serem colocadas em um objeto de reconhecimento
+worldfactor <- unlist(lapply(worldselect, is.factor))
+# Salvar em nova base apenas colunas reconhecidas como fatores do banco original
+worldfactor <- worldselect[ ,worldfactor]
+# Tranformação da base com fatores em dummy
+worlddummy <- acm.disjonctif(worldfactor)
+
+library(forcats)
+
+# Contar frequência de categorias de fatores.
+fct_count(worldfactor$religoin)
+# Reclassificar casos para maior frequência, segunda maior ferquência e outros.
+fct_lump(worldfactor$religoin, n = 2)
+
+######################################
