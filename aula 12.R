6bdc957fe6ca5907f78b465af86230f8bdcf8214
Leticia Souza <leticia.suely@ufpe.br>
2021-06-15 17:45:01 -0300

    Aula 12 e Atividade

--- aula 12.R
+++ aula 12.R
@@ -22,4 +22,28 @@ aula12_modificado <- gsub("15/06", "15-06", aula12_total$all_text)
 data_hifen <- str_extract_all(aula12_modificado, "\\d{2}-\\d{2}")
 
 
+########################################################
+
+nome <- c("Leticia", "Danillo", "Mariana", "Daniel", "Caio")
+curso <- c("Ciência Política", "Relações Internacionais", "Ciência Política"," Direito", "Ciência Política")
+idade <- c(23, 22, 22, 24, 33)
+país <- c("Colômbia", "Brasil", "Canadá", "França", "França")
+
+base1 <- data.frame(nome, curso, idade, país)
+
+nome <- c("leticia s", "danillo a", "mariana b", "daniel d", "caio m")
+inst <- c("UFPE", "Estácio", "UFPE", "UNICAP", "UFPE")
+
+base2 <- data.frame(nome, inst)
+
+# Unindo bases diferentes por aprximação de padrão de caracteres
+library(fuzzyjoin)
+
+base3 <- stringdist_join(base1, base2, mode='left')
+
+# Criando uma nova base com nova variável dada a presença de uma categoria na variável 'curso'
+base4 <- base3 %>% mutate(tag_CP = ifelse(grepl(paste("Ciência Política", collapse="|"), curso), '1', '0'))
+
+                       
+
 
